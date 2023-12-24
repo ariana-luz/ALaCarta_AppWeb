04de6e824c4d9d536b75524aba90fcaddf80603c
Ariana Osuna Vargas <osunavargasariana@gmail.com>
2023-12-24 09:46:42 -0300

    Nuevo path componente platos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { PerfilComponent } from './componentes/perfil/perfil.component';
 import { BuscadorComponent } from './componentes/buscador/buscador.component';
+import { PlatosComponent } from './componentes/platos/platos.component';
 
 export const routes: Routes = [
     {path: 'home', component:HomeComponent},
     {path: 'login', component:LoginComponent},
     {path: 'perfil', component:PerfilComponent},
     {path: 'buscador', component:BuscadorComponent},
+    {path: 'platos', component:PlatosComponent},
     {path: '', redirectTo:'login', pathMatch: 'full'}
 ];
