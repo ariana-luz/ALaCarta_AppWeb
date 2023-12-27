8118ce1fc5268ea445e0c22e289456119e5808b7
Ariana Osuna Vargas <osunavargasariana@gmail.com>
2023-12-27 10:10:06 -0300

    integración de Footer y Header a los componentes Perfil, Home y Buscador

--- src/app/componentes/buscador/buscador.component.ts
+++ src/app/componentes/buscador/buscador.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
-  selector: 'app-buscador',
-  standalone: true,
-  imports: [],
-  templateUrl: './buscador.component.html',
-  styleUrl: './buscador.component.scss'
+    selector: 'app-buscador',
+    standalone: true,
+    templateUrl: './buscador.component.html',
+    styleUrl: './buscador.component.scss',
+    imports: [FooterComponent, HeaderComponent]
 })
 export class BuscadorComponent {
 
--- src/app/componentes/perfil/perfil.component.ts
+++ src/app/componentes/perfil/perfil.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
-  selector: 'app-perfil',
-  standalone: true,
-  imports: [],
-  templateUrl: './perfil.component.html',
-  styleUrl: './perfil.component.scss'
+    selector: 'app-perfil',
+    standalone: true,
+    templateUrl: './perfil.component.html',
+    styleUrl: './perfil.component.scss',
+    imports: [FooterComponent, HeaderComponent]
 })
 export class PerfilComponent {
+    username = 'Ariana';
 
 }
